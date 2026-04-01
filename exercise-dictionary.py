2c84970d2585081b14634f47fd92c14980bf9901
karentrasporte <karentrasporte@gmail.com>
2026-04-01 22:24:40 +0800

    Enhanced the exercise-dictrionary.py to retrieve the definition from dictionary csv file

--- exercise-dictionary.py
+++ exercise-dictionary.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
+
+df = pd.read_csv("dictionary.csv")
 
 
 @app.route("/dict")
@@ -8,12 +11,12 @@ def dict_home():
     return render_template("dict.html")
 
 
-
 @app.route("/api/v1/<word>")
 def dict(word):
-    print(word.upper())
-    return {"definition":word.upper(),"word":word}
+    definition = df.loc[df['word'] == word]['definition'].squeeze()
+    response = {"definition":definition,"word":word}
+    return response
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
